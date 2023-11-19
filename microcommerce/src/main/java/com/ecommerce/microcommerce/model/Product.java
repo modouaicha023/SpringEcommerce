bcf7ed42c8160a7a6593921348673a515aac9549
Mendel Ba <33570785+MENDEL-BA@users.noreply.github.com>
2023-11-19 19:41:30 +0100

    Update Product.java

--- microcommerce/src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ microcommerce/src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -5,12 +5,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@JsonIgnoreProperties({"purchasePrice","id"})
-@JsonFilter("myDynamicFilter")
 public class Product {
     private int id;
     private String name;
     private int price;
-//    @JsonIgnore
+    @JsonIgnore
     private int purchasePrice;
 
     public Product() {
